e713645474ab52bcc06f7f11fa5e3a3d6b82a0db
Ben Monahan <101078308+M00N4RC@users.noreply.github.com>
2023-02-13 19:07:20 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -318,7 +318,7 @@ function draw() {
 
   //sizing and position of GIF and image of arc rings in center circle
   push();
-  ing.position(windowWidth / 2.47, 400);
+  ing.position(windowWidth / 2.60, 449);
   ing.size(388, 390);
   pop();
 
